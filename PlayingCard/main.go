951e4f4527453806d537273c3eed75ff3968d513
EputraP <EputraP@gmail.com>
2023-05-19 05:29:21 +0700

    update

---
+++ PlayingCard/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var card string = "ace of spades"
+	test := 20
+	card1 := "gini juga bisa"
+	fmt.Println(card)
+	fmt.Println(card1)
+	card1 = "changed"
+	fmt.Println(test)
+	fmt.Println(card1)
+}

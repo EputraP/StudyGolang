d93c79a2c24a84fb175aebf17b3879ba2ab14418
EputraP <EputraP@gmail.com>
2023-05-23 08:17:53 +0700

    update

--- PlayingCard/deck.go
+++ PlayingCard/deck.go
@@ -6,8 +6,28 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
-  func (d deck)print(){
-	for i, card := range d{
+func newDeck() deck{
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clover"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits{
+		for _, value := range cardValues{
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+  func (cards1 deck)print(){
+	for i, card := range cards1{
 		fmt.Println(i,card)
+	}
+  }
+  func (cards2 deck)print2(){
+	for i, card := range cards2{
+		fmt.Println(i,card,"cards2")
 	}
   }
--- PlayingCard/main.go
+++ PlayingCard/main.go
@@ -1,32 +1,38 @@
 package main
 
-import "fmt"
-
 func main() {
-	var card string = "ace of spades"
-	test := 20
-	card1 := "gini juga bisa"
-	fmt.Println(card)
-	fmt.Println(card1)
-	card1 = "changed"
-	fmt.Println(test)
-	fmt.Println(card1)
-	newCard1 := NewCard()
-	fmt.Println(newCard1)
-	numberCard:= NumberCard();
-	fmt.Println(numberCard)
+	// var card string = "ace of spades"
+	// test := 20
+	// card1 := "gini juga bisa"
+	// fmt.Println(card)
+	// fmt.Println(card1)
+	// card1 = "changed"
+	// fmt.Println(test)
+	// fmt.Println(card1)
+	// newCard1 := NewCard()
+	// fmt.Println(newCard1)
+	// numberCard:= NumberCard();
+	// fmt.Println(numberCard)
 
-	//slice
-	cards := []string{"ace of diamonds", NewCard()}
-	cards = append(cards, "six of spades")
-	for i, card:= range cards{
-		fmt.Println(i,card)
-	}
+	// //slice
+	// cards := []string{"ace of diamonds", NewCard()}
+	// cards = append(cards, "six of spades")
+	// for i, card:= range cards{
+	// 	fmt.Println(i,card)
+	// }
 
 	
-	cards1 := deck{"ace of diamonds", NewCard()}
-	cards1 = append(cards1, "six of spades")
-	cards1.print()
+	// cards1 := deck{"ace of diamonds", NewCard()}
+	// cards1 = append(cards1, "six of spades")
+	// cards1.print()
+
+	// cards2 := deck{"ace of diamonds", NewCard()}
+	// cards2 = append(cards1, "six of spades")
+	// cards2.print2()
+
+	cardsList := newDeck()
+	cardList.print()
+
 }
 
 func NewCard() string {
